92326a0cd8295f54b1e8bb173032943eefe177bf
luiz feliphi <luizfeliphilemos@gmail.com>
2026-08-27 17:20:30 -0300

    first commit

--- src/Lista1/num19.java
+++ src/Lista1/num19.java
@@ -1,14 +1,21 @@
 package Lista1;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class num19 {
-    public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
-        System.out.println();
-        int[] dado = new int[6];
-        for (int i = 0; i < 100; i++) {
-
+    public class Main {
+        public static void main(String[] args) {
+            Random random = new Random();
+            int[] contagem = new int[6];
+            for (int i = 0; i < 100; i++) {
+                int resultado = random.nextInt(6) + 1;
+                contagem[resultado - 1]++;
+            }
+            System.out.println("Resultado dos 100 lançamentos:");
+            for (int i = 0; i < contagem.length; i++) {
+                System.out.println("Face " + (i + 1) + ": " + contagem[i] + " vezes");
+            }
         }
     }
 }
