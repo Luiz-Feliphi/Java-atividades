fb56dda593a6a42525efa830dafed81eeedfbad6
luiz feliphi <luizfeliphilemos@gmail.com>
2026-08-27 17:23:15 -0300

    fix: questão 19 da lista 1

--- src/Lista1/num19.java
+++ src/Lista1/num19.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class num19 {
-    public class Main {
-        public static void main(String[] args) {
-            Random random = new Random();
-            int[] contagem = new int[6];
-            for (int i = 0; i < 100; i++) {
-                int resultado = random.nextInt(6) + 1;
-                contagem[resultado - 1]++;
-            }
-            System.out.println("Resultado dos 100 lançamentos:");
-            for (int i = 0; i < contagem.length; i++) {
-                System.out.println("Face " + (i + 1) + ": " + contagem[i] + " vezes");
-            }
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] contagem = new int[6];
+        for (int i = 0; i < 100; i++) {
+            int resultado = random.nextInt(6) + 1;
+            contagem[resultado - 1]++;
+        }
+        System.out.println("Resultado dos 100 lançamentos:");
+        for (int i = 0; i < contagem.length; i++) {
+            System.out.println("Face " + (i + 1) + ": " + contagem[i] + " vezes");
         }
     }
 }
