9b0091e22ebf1be51616095dce9c8cc5759bf1e2
luiz feliphi <luiz.lemos@estudante.ifgoiano.edu.br>
2026-09-11 08:03:19 -0300

    add: test do github

--- src/Lista2/num4.java
+++ src/Lista2/num4.java
@@ -2,25 +2,30 @@ package Lista2;
 
 public class num4 {
     public static void main(String[] args) {
-        double piAproximado = 0.0;
+       // double piAproximado = 0.0;
         double denominador = 1.0;
         boolean somar = true;
         boolean fim = true;
+        double piAtual =0.0;
+        double piAnterior=0.0;
         System.out.println("Calculando Pi infinitamente. Pressione Ctrl+C para parar.");
         while (fim) {
             if (somar) {
-                double diferenca=0;
-                if (diferenca == 0.05) {
-
-                }
-                piAproximado += (4.0 / denominador);
-                double piAtual = piAproximado;
+                piAtual += (4.0 / denominador);
+                //piAproximado = piAtual;
             } else {
-                piAproximado -= (4.0 / denominador);
+                piAtual -= (4.0 / denominador);
+                piAnterior = piAtual;
+
             }
             somar = !somar;
             denominador += 2.0;
-            System.out.println("Pi aproximado: " + piAproximado);
+            System.out.println("Pi aproximado: " + piAtual);
+            if (piAtual-piAnterior == 0.05){
+                fim=!fim;
+                System.out.print(piAtual-piAnterior);grughrugurfkkf
+            }
+
         }
     }
 }
